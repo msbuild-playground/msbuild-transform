a2a6c09142c19183533d8aab100814446e570608
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-03-04 14:36:50 +0700

    fixed pause command

--- Transform/ScanStation-Install.bat
+++ Transform/ScanStation-Install.bat
@@ -7,12 +7,14 @@ rem "== DO NOT TOUCH THIS =="
 set tempFile=%temp%/app.config
 set appConfig=%targetDir%/%app%/%app%.exe.config
 set projectFile="\\192.168.0.115\B Circle Software\NugetPackages\Transform.xml"
-rem set projectFile="Transform.xml"
+
+rem "== RREPARE ENV =="
 cp "%appConfig%" "%tempFile%"
+rmdir /S "%targetDir%/%app%"
 
-nuget install %app% -ExcludeVersion -OutputDirectory %targetDir%
-
-pymsbuild %projectFile% ^
+rem "== PROCESS =="
+call nuget install %app% -ExcludeVersion -OutputDirectory %targetDir%
+call pymsbuild %projectFile% ^
 	/p:SourceFile=%tempFile% ^
 	/p:TransformFile="%cd%/%transformer%" ^
 	/p:TargetFile=%appConfig% ^
